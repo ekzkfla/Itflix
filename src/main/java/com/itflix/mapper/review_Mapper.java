9535e73b43dd7d4d1a12409a6d9f22610b5653db
yujihun <yujihun@DESKTOP-VHIRAAO>
2022-09-28 19:30:26 +0900

    mapper 껍데기 추가

---
+++ src/main/java/com/itflix/mapper/review_Mapper.java
@@ -0,0 +1,31 @@
+package com.itflix.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface review_Mapper {
+	/*
+	 * 메쏘드이름은 	GuestMapper.xml 파일의 id와일치
+	 * 메쏘드인자타입은 GuestMapper.xml 파일의 parameterType 와일치
+	 * 메쏘드리턴타입은 GuestMapper.xml 파일의 resultType 와일치
+	 */
+	/*
+	<select id="selectByNo" resultType="com.itwill.guest.Guest" 
+	parameterType="_int">
+		select * from guest where guest_no=#{guest_no}
+	</select>
+	 */
+	public review_Mapper selectByNo(int no);
+	/*
+	<select id="selectAll" resultType="com.itwill.guest.Guest">
+		select * from guest
+	</select>
+	 */
+	/*
+	public List<user_info> selectAll();
+	public int insertGuest(user_info guest);
+	public int deleteGuest(int guest_no);
+	public int updateGuest(Guest guest);
+	*/
+}
