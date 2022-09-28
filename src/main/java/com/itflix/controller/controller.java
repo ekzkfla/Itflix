4740fd4325dd7719e2415eb0f6f21c1c677bb8ba
yujihun <yujihun@DESKTOP-VHIRAAO>
2022-09-28 16:13:19 +0900

    ..

--- src/main/java/com/itflix/controller/controller.java
+++ src/main/java/com/itflix/controller/controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class controller {
 	public controller() {
-		System.out.println("기본!!");
+		System.out.println("기본!!!");
 	}
 	//ItFlix 메인페이지
 	@RequestMapping(value = "main")
