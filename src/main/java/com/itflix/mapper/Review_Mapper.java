f310e6d553a9e72076fdc23d1b4cf0ef09df358a
KimHeeCheul <kim11403@naver.com>
2022-09-29 16:15:25 +0900

    Review Mapper ~ service

--- src/main/java/com/itflix/mapper/Review_Mapper.java
+++ src/main/java/com/itflix/mapper/Review_Mapper.java
@@ -1,6 +1,5 @@
 package com.itflix.mapper;
 
-
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
@@ -13,29 +12,33 @@ import com.itflix.dto.Review;
 
 @Mapper
 public interface Review_Mapper {
-	
-	//리뷰 목록 전체 출력
+
+	// 리뷰 목록 전체 출력
 	@Select("select * from review")
 	public List<Review> selectAll();
-	
-	//최신 리뷰 출력
+
+	// 본인 리뷰 보기(회원아이디 리뷰 보기)
+	@Select("select r.r_no, u.u_email, r.r_title, r.r_content, r.r_grade, r.r_date from user_info u join review r on u.u_email=r.u_email")
+	public List<Review> selectWroteReview();
+
+	// 최신 리뷰 출력
 	@Select("select r.r_no, u.u_email, r.r_title, r.r_content, r.r_grade, r.r_date from user_info u join review r on u.u_email = r.u_email order by r.r_date desc")
 	public List<Review> selectLatest();
-	
-	//리뷰 번호를 선택하여 1개 출력
+
+	// 리뷰 번호를 선택하여 1개 출력
 	@Select("select * from review where r_no=#{r_no}")
 	public Review selectByNo(int r_no);
-	
-	//리뷰 추가
-	@Insert("insert into review VALUES('#{r_no','#{r_title}','#{r_content}','#{r_grade}','#{r_date}','#{r_groupno}','#{r_step}','#{r_depth}','#{m_no}','#{u_email}')")
+
+	// 리뷰 추가
+	@Insert("insert into review VALUES('#{r_no}','#{r_title}','#{r_content}','#{r_grade}','#{r_date}','#{r_groupno}','#{r_step}','#{r_depth}','#{m_no}','#{u_email}')")
 	public int insertReview(Review review);
-	
-	//리뷰 수정
+
+	// 리뷰 수정
 	@Update("update review set r_title='#{r_title}',r_content='#{r_content}',r_grade='#{r_grade}',m_no='#{m_no}' where r_no='#{r_no}'")
 	public int updateReview(Review review);
-	
-	//리뷰 삭제
+
+	// 리뷰 삭제
 	@Delete("delete from review where r_no='#{r_no}'")
 	public int deleteReview(int no);
-	
+
 }
--- src/main/java/com/itflix/service/ReviewService.java
+++ src/main/java/com/itflix/service/ReviewService.java
@@ -11,6 +11,9 @@ public interface ReviewService {
 	// 리뷰 전체 출력
 	List<Review> selectAll() throws Exception;
 
+	// 본인 리뷰 보기(회원아이디 리뷰 보기)
+	List<Review> selectWroteReview() throws Exception;
+
 	// 최신 리뷰 출력
 	List<Review> selectLatest() throws Exception;
 
