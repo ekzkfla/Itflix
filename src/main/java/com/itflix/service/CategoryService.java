a3862389f087f9156b38a0c235ea389b1d76183a
Harim <godtoether94@naver.com>
2022-09-29 10:05:23 +0900

    .29 category mapper 수정 -하림-

--- src/main/java/com/itflix/service/CategoryService.java
+++ src/main/java/com/itflix/service/CategoryService.java
@@ -1,5 +1,24 @@
 package com.itflix.service;
 
+import java.util.List;
+
+import com.itflix.dto.Category;
+
 public interface CategoryService {
+	
+	//카테고리 번호를 이용한 해당 영화 리스트 출력
+	List<Category> selectByNoMovieList(int no) throws Exception;
+	//카테고리 전체 출력
+	List<Category> selectAll() throws Exception;
+	//카테고리 번호를 이용한 찾기
+	Category selectByNo(int no) throws Exception;
+	//카테고리 이름을 이용한 찾기
+	Category selectByName(String name) throws Exception;
+	//카테고리 추가
+	int insertCategory(Category category) throws Exception;
+	//카테고리 변경
+	int updateCategory(Category category) throws Exception;
+	//카테고리 삭제
+	int deleteCategory(int no) throws Exception;
 
 }
--- src/main/java/com/itflix/service/CategoryServiceImpl.java
+++ src/main/java/com/itflix/service/CategoryServiceImpl.java
@@ -18,19 +18,46 @@ public class CategoryServiceImpl implements CategoryService{
 		
 	}
 	
+	//카테고리 번호를 이용한 해당 영화 리스트 출력
+	@Override
 	public List<Category> selectByNoMovieList(int no) throws Exception{
 		return categoryDao.selectByNoMovieList(no);
 	}
 	
+	//카테고리 전체 출력
+	@Override
 	public List<Category> selectAll() throws Exception{
 		return categoryDao.selectAll();
 	}
 	
+	//카테고리 번호를 이용한 찾기
+	@Override
 	public Category selectByNo(int no)throws Exception {
 		return categoryDao.selectByNo(no);
 	}
 	
+	//카테고리 이름을 이용한 찾기
+	@Override
+	public Category selectByName(String name)throws Exception{
+		return categoryDao.selectByName(name);
+	}
 	
+	//카테고리 추가
+	@Override
+	public int insertCategory(Category category)throws Exception{
+		return categoryDao.insertCategory(category);
+	}
 	
+	//카테고리 변경
+	@Override
+	public int updateCategory(Category category)throws Exception{
+		return categoryDao.updateCategory(category);
+	}
+	
+	//카테고리 삭제
+	@Override
+	public int deleteCategory(int no)throws Exception{
+		return categoryDao.deleteCategory(no);
+	}
 	
 }
