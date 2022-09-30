788622b14146fb2d7405dc3262d4e3720093ddaf
KimHeeCheul <kim11403@naver.com>
2022-09-30 15:10:12 +0900

    review 수정중

--- src/main/java/com/itflix/test/ReviewServiceTestApplication.java
+++ src/main/java/com/itflix/test/ReviewServiceTestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.itflix.dao.CategoryDao;
 import com.itflix.dao.ReviewDao;
+import com.itflix.dto.Review;
 import com.itflix.service.CategoryService;
 import com.itflix.service.NoticeService;
 
@@ -34,9 +35,16 @@ public class ReviewServiceTestApplication {
 		ConfigurableApplicationContext context=application.run(args);
 		ReviewDao reviewDao = context.getBean(ReviewDao.class);
 		 System.out.println(reviewDao);
-		 System.out.println(reviewDao.selectAll());
-		 System.out.println(reviewDao.selectByNo(3));
-		 System.out.println(reviewDao.deleteReview(11));
+		 //System.out.println(reviewDao.selectAll());
+		 //System.out.println(reviewDao.selectByNo(3));
+		 //System.out.println(reviewDao.deleteReview(11));
+		 System.out.println("check"+reviewDao.selectWroteReview());
+		 //System.out.println("check2"+reviewDao.selectLatest());
+		 //System.out.println("check3"+reviewDao.updateReview(new Review(3, "수정확인", "수정확인중", 50, 20220930, 0, 0, 0, 3, null)));
+													 /*(int r_no, String r_title, String r_content,
+													  *  int r_grade, Date r_date, int r_groupno,
+													  *   int r_step, int r_depth, Movie movie, User_Info user_Info)*/
+		 //System.out.println(reviewDao.insertReview(new Review(25, "abc", "abdc", 5, "2022/10/11", 1, 1, 1, 3, "guard4@gmail.com")));
 		 
 	}
 }
