7aeb98378d22131ad56f1b9303b5cbdca712cbe9
wkqrl <wkqrl@221.146.149.73>
2022-09-30 15:55:54 +0900

    subscription 매퍼 dao impl 수정/작성

--- src/main/java/com/itflix/dao/SubscriptionDaoImpl.java
+++ src/main/java/com/itflix/dao/SubscriptionDaoImpl.java
@@ -1,5 +1,59 @@
 package com.itflix.dao;
 
-public class SubscriptionDaoImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itflix.dto.Subscription;
+import com.itflix.mapper.Subscription_Mapper;
+
+@Repository
+public class SubscriptionDaoImpl implements SubscriptionDao{
+
+	@Autowired
+	private Subscription_Mapper subscruption_Mapper;
+	
+	public SubscriptionDaoImpl() {
+		
+	}
+	
+	public SubscriptionDaoImpl(Subscription_Mapper subscription_Mapper) {
+		this.subscruption_Mapper = subscription_Mapper;
+	}
+	
+	public void setSubscription_Mapper(Subscription_Mapper subscription_Mapper) {
+		this.subscruption_Mapper = subscription_Mapper;
+	}
+
+	//전체 구독자리스트
+	@Override
+	public List<Subscription> selectlistAll() throws Exception {
+		List<Subscription> subscriptionList = subscruption_Mapper.selectlistAll();
+		return subscriptionList;
+	}
+	
+
+	//구독시작날짜 찾기
+	@Override
+	public Subscription selectByStDate(String u_email) throws Exception {
+		Subscription s_start = subscruption_Mapper.selectByStDate(u_email);
+		return s_start;
+	}
+
+	//구독만료날짜 찾기
+	@Override
+	public Subscription selectByEndDate(String u_email) throws Exception {
+		Subscription s_end = subscruption_Mapper.selectByStDate(u_email);
+		return s_end;
+	}
+
+	//구독자 단일 찾기
+	@Override
+	public Subscription selectByNo(String u_email) throws Exception {
+		Subscription s_no = subscruption_Mapper.selectByStDate(u_email);
+		return s_no;
+	}
+	
+	
 }
--- src/main/java/com/itflix/mapper/Subscription_Mapper.java
+++ src/main/java/com/itflix/mapper/Subscription_Mapper.java
@@ -1,31 +1,29 @@
 package com.itflix.mapper;
 
+import java.util.List;
 
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.itflix.dto.Subscription;
 
 @Mapper
 public interface Subscription_Mapper {
-	/*
-	 * 메쏘드이름은 	GuestMapper.xml 파일의 id와일치
-	 * 메쏘드인자타입은 GuestMapper.xml 파일의 parameterType 와일치
-	 * 메쏘드리턴타입은 GuestMapper.xml 파일의 resultType 와일치
-	 */
-	/*
-	<select id="selectByNo" resultType="com.itwill.guest.Guest" 
-	parameterType="_int">
-		select * from guest where guest_no=#{guest_no}
-	</select>
-	 */
-	public Subscription_Mapper selectByNo(int no);
-	/*
-	<select id="selectAll" resultType="com.itwill.guest.Guest">
-		select * from guest
-	</select>
-	 */
-	/*
-	public List<user_info> selectAll();
-	public int insertGuest(user_info guest);
-	public int deleteGuest(int guest_no);
-	public int updateGuest(Guest guest);
-	*/
+	
+	//전체 구독자리스트
+	@Select("select * from Subscription order by s_no ASC")
+	public List<Subscription> selectlistAll();
+	
+	//구독시작날짜 찾기
+	@Select("select * from Subscription where u_email = #{u_email}")
+	public Subscription selectByStDate(String u_email);
+	
+	//구독만료날짜 찾기
+	@Select("select * from Subscription where u_email = #{u_email}")
+	public Subscription selectByEndDate(String u_email);
+	
+	//구독자 단일 찾기
+	@Select("select * from Subscription where u_email = #{u_email}")
+	public Subscription selectByNo(String u_email);
 }
