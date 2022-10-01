911e9810f896989d7fa11d5e8af239f7dc79f0d5
Harim <godto@DESKTOP-5CJT6FD>
2022-10-01 15:32:08 +0900

    10.1 selectTotal category수정 -하림

--- src/main/java/com/itflix/domain/select Total.sql
+++ src/main/java/com/itflix/domain/select Total.sql
@@ -432,6 +432,15 @@ group by c.cg_name,
        m.m_actor,
        m.m_info,
        m.m_date;
+       
+--카테고리별  영화 한개 상세 상세페이지 검색 (리뷰 추가 -리뷰없을시 null표시)
+select * from(select c.cg_no,c.cg_name, m.m_name,m.m_actor,m.m_info,m.m_image,m.m_date,m.m_url,r.r_title,r.r_content,r.r_date,avg(r.r_grade)
+from category c  
+join movie m on m.cg_no = c.cg_no 
+left JOIN review r on r.m_no = m.m_no
+where c.cg_no = 4 AND m.m_no=19
+GROUP BY c.cg_no,c.cg_name, m.m_name,m.m_actor,m.m_info,m.m_image,m.m_date,m.m_url,r.r_title,r.r_content,r.r_date
+ORDER BY r.r_date DESC) where ROWNUM<=1;
 
 ------------------------------REVIEW SELECT-----------------------------------
 
