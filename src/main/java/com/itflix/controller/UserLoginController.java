2975f11a792a0f12875fc7447b7db1301644e02e
sojin_an <sojin_an@naver.com>
2022-10-05 17:45:07 +0900

    user login controller :쏘

--- src/main/java/com/itflix/controller/UserLoginController.java
+++ src/main/java/com/itflix/controller/UserLoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itflix.dto.User_Info;
 import com.itflix.service.User_InfoService;
 
 @Controller
@@ -19,11 +20,31 @@ public class UserLoginController {
 	@RequestMapping(value = "user_login_action")
 	public String user_login_action(HttpServletRequest request, HttpSession session) {
 		String u_email = request.getParameter("u_emil");
-		
-		
-		
-		
-		return null;
+		String u_pass = request.getParameter("u_pass");
+		String forwardPath = "";
+		try {
+			/*
+			 *  1:성공 
+			 * -1:비번 불일치 
+			 * -2:email 미존재
+			 */
+		int result = user_InfoService.login(u_email, u_pass);
+		if(result == 1) {
+			User_Info loginUser = user_InfoService.selectByEmail(u_email);
+			session.setAttribute("login_email", loginUser.getU_email());;
+			session.setAttribute("login_user", loginUser);			
+			forwardPath = "";
+		} else if (result == -1) {
+			request.setAttribute("login_id", u_email);
+		} else if(result == -2) {
+			request.setAttribute("login_id", u_email);
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+		return "error";
+	}
+		return forwardPath;	
+
 	}
 	
 
