03ea6e7d24ceb56b0c5de6ce6c61cad4ada711c8
yujihun <yujihun@192.168.1.144>
2022-10-06 18:05:35 +0900

    top 수정 진행 중 -지훈-

--- src/main/java/com/itflix/controller/UserProfileController.java
+++ src/main/java/com/itflix/controller/UserProfileController.java
@@ -13,33 +13,9 @@ import com.itflix.service.User_InfoService;
 
 @Controller
 public class UserProfileController {
-/*
+
 	@Autowired
 	private User_InfoService user_InfoService;
-	//로그인 세션 체크
-	@LoginCheck
-	@PostMapping("/user_session_check")
-	public Map user_session_check(HttpSession session) throws Exception {
-		Map resultMap = new HashMap();
-		int code = 1;
-		String url = "main";
-		String msg = "세션존재";
-		List<User> resultList = new ArrayList<User>();
+	
 
-		String sUserId = (String) session.getAttribute("sUserId");
-		User sUser = userService.findUser(sUserId);
-		resultList.add(sUser);
-
-		resultMap.put("code", code);
-		resultMap.put("url", url);
-		resultMap.put("msg", msg);
-		resultMap.put("data", resultList);
-		return resultMap;
-	}
-			
-		
-		
-		return forwardPath;
-	}
-*/
 }
