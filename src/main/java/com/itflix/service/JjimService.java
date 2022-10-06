5d2143836117d90209794c91f41c1e610aa9dd6c
sojin_an <sojin_an@naver.com>
2022-10-06 15:09:02 +0900

    jjim mapper dao service 수정 완료

--- src/main/java/com/itflix/service/JjimService.java
+++ src/main/java/com/itflix/service/JjimService.java
@@ -6,6 +6,9 @@ import com.itflix.dto.Jjim;
 
 public interface JjimService {
 	
+	/* 1명 회원 영화 찜 목록보기(리뷰평점포함) */
+	List<Jjim> jjimList(String u_email) throws Exception;
+	
 	/* 영화 찜하기 */
 	int jjimInsert(String u_email,int m_no) throws Exception;
 
@@ -15,7 +18,5 @@ public interface JjimService {
 	/* 리뷰의 총평점 조인안하고 불러오기*/
 	List<Jjim> jjimListTest5(String u_email) throws Exception;
 	
-	/* 리뷰의 총평점 조인안하고 불러오기*/
-	List<Jjim> jjimList(String u_email) throws Exception;
 	
 }
