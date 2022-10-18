2f3cd31805780fc804719bc1a32908ab04aa5f72
Harim <godtoether94@naver.com>
2022-10-18 17:57:32 +0900

    landing 수정 -하림-

--- src/main/java/com/itflix/controller/MainController.java
+++ src/main/java/com/itflix/controller/MainController.java
@@ -294,12 +294,13 @@ public class MainController {
 		if(subscriptUser==null) {
 			//구독권이 없는 경우 
 			int t_no=1;
-
+			session.invalidate();
 			subscriptonService.insertSubscription(0, null, null, s_cardName,Integer.parseInt( s_cardNumberfirst),t_no, user_Info.getU_email());
-			msg ="결제 완료";
-			request.setAttribute("msg", msg);
-			forwardPath="redirect:main";
+			msg ="결제 완료! 다시 로그인 해주세요.";
 			
+			request.setAttribute("msg", msg);
+			request.setAttribute("url", "main");
+			return "alert";
 		}else if(subscriptUser !=null) {
 			//구독권이 있거나 예전에 구매한 기록이 있을 경우
 			int t_no=1;
