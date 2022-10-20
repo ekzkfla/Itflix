dc1a2717c5bb60080e23fe07770d718842817ba5
SJ <SJ@DESKTOP-R9O5P0Q>
2022-10-20 17:47:36 +0900

    이미지 업로드 테스트중

--- src/main/java/com/itflix/controller/MainController.java
+++ src/main/java/com/itflix/controller/MainController.java
@@ -252,46 +252,6 @@ public class MainController {
 	}
 	
 	
-    @PostMapping("/insertImage")
-    public String upload(@RequestParam("movieImage") MultipartFile file) {
- 
-        System.out.println("파일 이름 : " + file.getOriginalFilename());
-        System.out.println("파일 크기 : " + file.getSize());
-        // 데이터 베이스에 정보를 저장하는 건
-        // 이렇게 file의 get 메소드를 활용해 필요한 정보들을 가져오고 
-        // 그걸 DTO에 담아 insert하면 된다. 
-        // 간단한거니 후의 과정은 생략하고 파일로 서버에 저장하는 걸 보자면
- 
-        try (
-                // 맥일 경우
-                // FileOutputStream fos = new FileOutputStream("/tmp/" +file.getOriginalFilename());
-                // 윈도우일 경우
-                FileOutputStream fos = new FileOutputStream("c:/tmp/" + file.getOriginalFilename());
-                // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
-                InputStream is = file.getInputStream();) {
-                // file로 부터 inputStream을 가져온다.
-            
-            int readCount = 0;
-            byte[] buffer = new byte[1024];
-            // 파일을 읽을 크기 만큼의 buffer를 생성하고
-            // ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
-            
-            while ((readCount = is.read(buffer)) != -1) {
-                //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
-                
-                fos.write(buffer, 0, readCount);
-                // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException("file Save Error");
-        }
- 
-        return "uploadok";
-    }
-
-	
-	
-	
 	//리뷰 리스트 페이지
 	@RequestMapping(value = "reviewlist",params = "m_no")
 	public String reviewlist(@RequestParam int m_no, Model model)throws Exception{
